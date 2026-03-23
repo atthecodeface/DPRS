e06509e24ce0425ace4a7c208fb51f8f0113fa49
cstarkjp <cstarkjp@gmail.com>
2026-03-23 12:28:27 +0800

    Refactoring: rename key rs files

--- src/dk.rs
+++ src/dk.rs
@@ -3,22 +3,22 @@
 // //!
 
 mod cell_model_1d;
-mod dk_model_1d;
+mod growth_model_1d;
 mod lattice_model_1d;
-mod run_simulation_1d;
+mod run_1d;
 mod simulation_1d;
 
 mod cell_model_2d;
-mod dk_model_2d;
+mod growth_model_2d;
 mod lattice_model_2d;
-mod run_simulation_2d;
+mod run_2d;
 mod simulation_2d;
 
 mod cell_model_3d;
-mod dk_model_3d;
+mod growth_model_3d;
 mod lattice_model_3d;
 mod nbrhood_3d;
-mod run_simulation_3d;
+mod run_3d;
 mod simulation_3d;
 
 pub use cell_model_3d::CellModel3D;
@@ -33,24 +33,12 @@ pub fn sim_dk(params: Parameters) -> (usize, Vec<Vec<DualState>>, Vec<Vec<f64>>,
     params.print();
     println!();
     let (t_run_time, n_lattices, lattices, tracking) = match (&params.processing, &params.dim) {
-        (Processing::Serial, Dimension::D1) => {
-            run_simulation_1d::run_simulation(&params, &Processing::Serial)
-        }
-        (Processing::Parallel, Dimension::D1) => {
-            run_simulation_1d::run_simulation(&params, &Processing::Parallel)
-        }
-        (Processing::Serial, Dimension::D2) => {
-            run_simulation_2d::run_simulation(&params, &Processing::Serial)
-        }
-        (Processing::Parallel, Dimension::D2) => {
-            run_simulation_2d::run_simulation(&params, &Processing::Parallel)
-        }
-        (Processing::Serial, Dimension::D3) => {
-            run_simulation_3d::run_simulation(&params, &Processing::Serial)
-        }
-        (Processing::Parallel, Dimension::D3) => {
-            run_simulation_3d::run_simulation(&params, &Processing::Parallel)
-        }
+        (Processing::Serial, Dimension::D1) => run_1d::run(&params, &Processing::Serial),
+        (Processing::Parallel, Dimension::D1) => run_1d::run(&params, &Processing::Parallel),
+        (Processing::Serial, Dimension::D2) => run_2d::run(&params, &Processing::Serial),
+        (Processing::Parallel, Dimension::D2) => run_2d::run(&params, &Processing::Parallel),
+        (Processing::Serial, Dimension::D3) => run_3d::run(&params, &Processing::Serial),
+        (Processing::Parallel, Dimension::D3) => run_3d::run(&params, &Processing::Parallel),
     };
     match params.processing {
         Processing::Serial => println!(
---
+++ src/dk/run_1d.rs
@@ -0,0 +1,74 @@
+// #![warn(missing_docs)]
+// //!
+// //!
+
+use crate::dk::simulation_1d::simulation;
+use crate::dk::{growth_model_1d, lattice_model_1d};
+use crate::parameters::{DualState, Parameters, Processing};
+use growth_model_1d::DKModel1D;
+use lattice_model_1d::LatticeModel1D;
+use rand::SeedableRng;
+use rand::rngs::StdRng;
+use std::time::Instant;
+
+/// Run a simulation and record how long the computation takes.
+pub fn run(
+    params: &Parameters,
+    processing: &Processing,
+) -> (f64, usize, Vec<Vec<DualState>>, Vec<Vec<f64>>) {
+    let dp_cell_model = DKModel1D::default();
+    // Buffer lattice edges
+    let pad: usize = match params.do_edge_buffering {
+        true => 1,
+        false => 0,
+    };
+    let pruned_n_x = params.n_x;
+    let n_x: usize = pruned_n_x + pad * 2;
+    let mut lattice_model_1d: LatticeModel1D<DKModel1D> =
+        LatticeModel1D::new(dp_cell_model, n_x, (DualState::Empty, DualState::Empty));
+
+    let mut rng = StdRng::seed_from_u64(params.seed as u64);
+    lattice_model_1d.randomized_lattice(&mut rng, params.p0);
+
+    // Set up thread pool of size set by user
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(params.n_threads)
+        .build()
+        .unwrap();
+
+    // Start the timer
+    let time = Instant::now();
+
+    // Do the simulation
+    let (n_lattices, lattices, tracking) = pool.install(|| {
+        simulation(
+            lattice_model_1d,
+            &mut rng,
+            processing,
+            &params,
+            params.n_iterations,
+            params.sample_period,
+        )
+    });
+    // Stop the clock
+    let duration: f64 = time.elapsed().as_secs_f64();
+
+    // If needed, remove edge buffering before returning the lattice time-slices.
+    let lattices = if params.do_edge_buffering {
+        // Step through each of the recorded lattices, pruning off by 'pad'
+        // at each edge, returning the pruned lattices
+        lattices
+            .into_iter()
+            .map(|lattice| {
+                let mut pruned_lattice = vec![];
+                pruned_lattice.extend_from_slice(&lattice[pad..(pad + pruned_n_x)]);
+
+                pruned_lattice
+            })
+            .collect()
+    } else {
+        lattices
+    };
+
+    (duration, n_lattices, lattices, tracking)
+}
