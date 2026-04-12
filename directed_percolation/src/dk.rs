6e0b44c592a1e18b8e26624597e31d588b7f08ab
cstarkjp <cstarkjp@gmail.com>
2026-04-12 10:42:12 +0800

    Simplify dk/ file names because fussy

--- directed_percolation/src/dk.rs
+++ directed_percolation/src/dk.rs
@@ -4,24 +4,24 @@
 mod traits;
 pub use traits::{Cell1D, Cell2D, Cell3D, CellModel};
 
-mod cell_nbrhood_2d;
-mod cell_nbrhood_3d;
-mod growth_model_1d;
-mod growth_model_2d;
-mod growth_model_3d;
-mod lattice_model_1d;
-mod lattice_model_2d;
-mod lattice_model_3d;
+mod nbrhood_2d;
+mod nbrhood_3d;
+mod growth_1d;
+mod growth_2d;
+mod growth_3d;
+mod lattice_1d;
+mod lattice_2d;
+mod lattice_3d;
 
 #[cfg(test)]
 mod tests;
 
-pub use cell_nbrhood_2d::{CellNbrhood2D, RowIterator2D};
-pub use cell_nbrhood_3d::{CellNbrhood3D, RowIterator3D};
-pub use growth_model_1d::{DKSimplified1D, DKStaggered1D};
-pub use growth_model_2d::{DKSimplified2D, DKStaggered2D};
-pub use growth_model_3d::DKSimplified3D;
+pub use nbrhood_2d::{CellNbrhood2D, RowIterator2D};
+pub use nbrhood_3d::{CellNbrhood3D, RowIterator3D};
+pub use growth_1d::{DKSimplified1D, DKStaggered1D};
+pub use growth_2d::{DKSimplified2D, DKStaggered2D};
+pub use growth_3d::DKSimplified3D;
 
-pub use lattice_model_1d::LatticeModel1D;
-pub use lattice_model_2d::LatticeModel2D;
-pub use lattice_model_3d::LatticeModel3D;
+pub use lattice_1d::LatticeModel1D;
+pub use lattice_2d::LatticeModel2D;
+pub use lattice_3d::LatticeModel3D;
