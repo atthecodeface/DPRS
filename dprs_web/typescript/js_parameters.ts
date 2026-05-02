1ab47fcad5399f8f873c526dcbc1051a09fb7026
cstarkjp <cstarkjp@gmail.com>
2026-05-02 10:38:52 +0800

    Implement callback on radio button select for presets

--- dprs_web/typescript/js_parameters.ts
+++ dprs_web/typescript/js_parameters.ts
@@ -183,7 +183,7 @@ export class JsParameters {
     this.params = new Params();
     this.topo = [new Topo(), new Topo(), new Topo()];
     this.dims = new Dims();
-    this.preset = 1;
+    this.preset = 0;
 
     this.topo[0]!.periodic = true;
     this.topo[1]!.periodic = true;
